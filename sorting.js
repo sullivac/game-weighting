f1e9d8cebd79b9f741cab58e56494bdd5da96e9e
Corey Sullivan <sullivac@fastmail.fm>
2021-03-04 16:01:18 +0000

    Refactor then application to sorting functions.

--- sorting.js
+++ sorting.js
@@ -11,31 +11,29 @@ function descending (left, right) {
 function thenBy (next) {
   const parent = this
 
-  function sort (left, right) {
+  return applyThen(function sort (left, right) {
     const parentResult = parent(left, right)
 
     return parentResult ? parentResult : next(left, right)
-  }
+  })
+}
 
+function sorter (direction) {
+  return function (selectPropertyOf) {
+    return applyThen(function sort (left, right) {
+      return direction(selectPropertyOf(left), selectPropertyOf(right))
+    })
+  }
+}
+
+function applyThen (sort) {
   sort.then = thenBy
 
   return sort
 }
 
-function sorter (direction) {
-  return function (selectPropertyOf) {
-    function sort (left, right) {
-      return direction(selectPropertyOf(left), selectPropertyOf(right))
-    }
-
-    sort.then = thenBy
-
-    return sort
-  }
-}
-
 module.exports = {
   descending,
-  byAscending: sorter(ascending),
-  byDescending: sorter(descending)
+  byAscending: applyThen(sorter(ascending)),
+  byDescending: applyThen(sorter(descending))
 }
