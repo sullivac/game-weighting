5306e84e8d84ad9dc9363d6e3e464044f50b054f
Corey Sullivan <sullivac@fastmail.fm>
2021-03-06 23:47:29 +0000

    Fix first time token acquisition.

--- googleOAuth2.js
+++ googleOAuth2.js
@@ -7,7 +7,8 @@ const { writeFile } = require('fs/promises')
 
 const { readJsonFile } = require('./readJsonFile')
 
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+const scope = ['https://www.googleapis.com/auth/spreadsheets']
+
 const oAuth2TokenPath = path.join(process.env.HOME, '.googleapis', 'token.json')
 const oAuth2ClientCredentialsPath = path.join(
   process.env.HOME,
@@ -15,12 +16,14 @@ const oAuth2ClientCredentialsPath = path.join(
   'credentials.json'
 )
 
-function setCredentialsOnClient (oAuth2Client) {
-  return function (credentials) {
-    oAuth2Client.setCredentials(credentials)
-
-    return oAuth2Client
+function initializeOAuth2Client ({
+  installed: {
+    client_id: clientId,
+    client_secret: clientSecret,
+    redirect_uris: redirectUris
   }
+}) {
+  return new google.auth.OAuth2(clientId, clientSecret, redirectUris[0])
 }
 
 function question (query) {
@@ -32,49 +35,52 @@ function question (query) {
   return new Promise(resolve =>
     rl.question(query, answer => {
       rl.close()
+
       resolve(answer)
     })
   )
 }
 
-function requestOAuth2Tokens (oAuth2Client) {
+function requestOAuth2Tokens (oAuth2Client, setCredentialsOnClient) {
   return function () {
     const authUrl = oAuth2Client.generateAuthUrl({
       access_type: 'offline',
-      scope: SCOPES
+      scope
     })
 
     console.log('Authorize this app by visiting this url:', authUrl)
 
     return question('Enter the code from that page here: ')
       .then(code => oAuth2Client.getToken(code))
-      .then(({ tokens }) => {
-        setCredentialsOnClient(tokens)
+      .then(writeToken(setCredentialsOnClient))
+  }
+}
 
-        return writeFile(oAuth2TokenPath, JSON.stringify(tokens))
-      })
+function setCredentials (oAuth2Client) {
+  return function (credentials) {
+    oAuth2Client.setCredentials(credentials)
+
+    return oAuth2Client
+  }
+}
+
+function writeToken (setCredentialsOnClient) {
+  return function ({ tokens }) {
+    return writeFile(oAuth2TokenPath, JSON.stringify(tokens)).then(() =>
+      setCredentialsOnClient(tokens)
+    )
   }
 }
 
 function initializeGoogleOAuth2Client (useOAuth2Client) {
-  return readJsonFile(oAuth2ClientCredentialsPath, data => data.installed)
-    .then(
-      ({
-        client_secret: clientSecret,
-        client_id: clientId,
-        redirect_uris: redirectUris
-      }) => {
-        const oAuth2Client = new google.auth.OAuth2(
-          clientId,
-          clientSecret,
-          redirectUris[0]
-        )
+  return readJsonFile(oAuth2ClientCredentialsPath, initializeOAuth2Client)
+    .then(oAuth2Client => {
+      const setCredentialsOnClient = setCredentials(oAuth2Client)
 
-        return readJsonFile(oAuth2TokenPath)
-          .then(setCredentialsOnClient(oAuth2Client))
-          .catch(requestOAuth2Tokens(oAuth2Client))
-      }
-    )
+      return readJsonFile(oAuth2TokenPath)
+        .then(setCredentialsOnClient)
+        .catch(requestOAuth2Tokens(oAuth2Client, setCredentialsOnClient))
+    })
     .then(useOAuth2Client)
 }
 
